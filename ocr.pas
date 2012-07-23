ed9c932c077208b76035d3ffc7bc684e244d6737
reiniero <reinierolislagers@gmail.com>
2012-07-23 14:21:33 +0200

    Store hocr text position

--- ocr.pas
+++ ocr.pas
@@ -41,6 +41,7 @@ type
 
   TOCR = class(TObject)
   private
+    FHOCRFile: string;
     FImageFile: string;
     FLanguage: string;
     FOrientation: Orientation;
@@ -49,6 +50,8 @@ type
   public
     // Perform the actual OCR
     procedure RecognizeText;
+    // File with hOCR: position and text in image
+    property HOCRFile: string read FHOCRFile;
     // Input image
     property ImageFile: string write FImageFile;
     property Language: string read FLanguage write FLanguage;
@@ -76,29 +79,43 @@ begin
   if FLanguage='' then FLanguage:='en'; //Default to English
   OutputFile:=GetTempFileName;
   FText:='';
+  FHOCRFile:='';
+  //todo: upside down detection? OCR 2x, higher detection rate is right orientation
+  //=> we can use tesseract 3 -pam parameter perhaps?
+  // tesseract sticks results in outputfile+.txt
   if ExecuteCommand(OCRCommand+' "'+FImageFile+'" "'+OutputFile+'" -l '+FLanguage,false)=0 then
   begin
-    writeln('Result:');
-    // tesseract sticks results in outputfile+.txt
-    //todo: upside down detection? OCR 2x, higher detection rate is right orientation
-    //=> we can use tesseract 3 -pam parameter perhaps?
     Results:=TStringList.Create;
     try
+      // By now we should have scan.tif, scan.txt and scan.html
       Results.LoadFromFile(OutputFile+'.txt');
       FText:=Results.Text;
+      // No more use for text output
+      Sysutils.DeleteFile(OutputFile+'.txt');
     finally
       Results.Free;
+    end;
+    // Output position & word text in hocr format:
+    if ExecuteCommand(OCRCommand+' "'+FImageFile+'" "'+OutputFile+'" -l '+FLanguage+' hocr',false)=0 then
+    begin
+      FHOCRFile:=OutputFile+'.html';
+      writeln('Result: hocr done: '+FHOCRFile);
+    end
+    else
+    begin
+      writeln('Error generating hocr.');
     end;
   end
   else
   begin
-    writeln('Error running command.');
+    writeln('Error performing tesseract OCR.');
   end;
 end;
 
 constructor TOCR.Create;
 begin
   inherited Create;
+  FHOCRFile:='';
   FOrientation:=orUnknown;
   FLanguage:='nld'; //Let's test with Dutch.
 end;
