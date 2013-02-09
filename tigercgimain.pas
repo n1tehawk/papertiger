85ed551e9ad500fe6b5205a0ee9ce10ed2f80b0f
reiniero <reiniero@localhost>
2013-02-09 16:35:56 +0100

    Started experiment with cgi

--- tigercgimain.pas
+++ tigercgimain.pas
@@ -6,10 +6,17 @@ interface
 
 uses
   SysUtils, Classes, httpdefs, fpHTTP, fpWeb,
-  scan, ocr, pdf, tigerdb, tigersettings, imagecleaner;
+  tigerservercore;
 
 type
+
+  { TFPWebModule1 }
+
   TFPWebModule1 = class(TFPWebModule)
+    procedure listRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
+    procedure unsupportedRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
   private
     { private declarations }
   public
@@ -22,6 +29,22 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFPWebModule1 }
+
+procedure TFPWebModule1.listRequest(Sender: TObject; ARequest: TRequest;
+  AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>To do: add list scans stuff here.</p>');
+  Handled:=true;
+end;
+
+procedure TFPWebModule1.unsupportedRequest(Sender: TObject; ARequest: TRequest;
+  AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>Unsupported method.</p>' );
+  Handled := true;
+end;
 
 initialization
   RegisterHTTPModule('TFPWebModule1', TFPWebModule1);
