265a1cc1baa525aa2884f144d8a63d7a39a2689c
reiniero <reinierolislagers@gmail.com>
2013-02-15 12:21:59 +0100

    Started some more http methods

--- tigercgimain.pas
+++ tigercgimain.pas
@@ -13,9 +13,17 @@ type
   { TFPWebModule1 }
 
   TFPWebModule1 = class(TFPWebModule)
+    procedure deletedocumentRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
     procedure listRequest(Sender: TObject; ARequest: TRequest;
       AResponse: TResponse; var Handled: Boolean);
+    procedure scanRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
+    procedure showdocumentRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
     procedure unsupportedRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
+    procedure uploadimageRequest(Sender: TObject; ARequest: TRequest;
       AResponse: TResponse; var Handled: Boolean);
   private
     { private declarations }
@@ -32,6 +40,13 @@ implementation
 
 { TFPWebModule1 }
 
+procedure TFPWebModule1.deletedocumentRequest(Sender: TObject;
+  ARequest: TRequest; AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>todo: support method '+ARequest.QueryString+'.</p>' );
+  Handled := true;
+end;
+
 procedure TFPWebModule1.listRequest(Sender: TObject; ARequest: TRequest;
   AResponse: TResponse; var Handled: Boolean);
 begin
@@ -39,10 +54,31 @@ begin
   Handled:=true;
 end;
 
+procedure TFPWebModule1.scanRequest(Sender: TObject; ARequest: TRequest;
+  AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>todo: support method '+ARequest.QueryString+'.</p>' );
+  Handled := true;
+end;
+
+procedure TFPWebModule1.showdocumentRequest(Sender: TObject;
+  ARequest: TRequest; AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>todo: support method '+ARequest.QueryString+'.</p>' );
+  Handled := true;
+end;
+
 procedure TFPWebModule1.unsupportedRequest(Sender: TObject; ARequest: TRequest;
   AResponse: TResponse; var Handled: Boolean);
 begin
   AResponse.Contents.Add('<p>Unsupported method.</p>' );
+  Handled := true;
+end;
+
+procedure TFPWebModule1.uploadimageRequest(Sender: TObject; ARequest: TRequest;
+  AResponse: TResponse; var Handled: Boolean);
+begin
+  AResponse.Contents.Add('<p>todo: support method '+ARequest.QueryString+'.</p>' );
   Handled := true;
 end;
 
