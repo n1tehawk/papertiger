29a34a0bfb39ed459a3c0eb15a011f950382ef9e
reiniero <reiniero@localhost>
2013-02-16 10:35:57 +0100

    Started logging support

---
+++ tigerutil.pas
@@ -0,0 +1,126 @@
+unit tigerutil;
+{ Utility functions such as logging support.
+
+  Copyright (c) 2012-2013 Reinier Olislagers
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to
+  deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+}
+
+
+
+{$i tigerserver.inc}
+
+interface
+uses
+  Classes, SysUtils, eventlog;
+
+type
+  { TLogger }
+  TLogger = class(TObject)
+  private
+    FLog: TEventLog; //Logging/debug output to file
+  public
+    // Write to log and optionally console with seriousness etInfo
+    procedure WriteLog(Message: string; ToConsole: Boolean=false);
+    // Write to log and optionally console with specified seriousness
+    procedure WriteLog(EventType: TEventType;Message: string; ToConsole: Boolean=false);
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+var
+  TigerLog: TLogger; //Created by unit initialization so available for every referencing unit
+
+//Shows non-debug messages on screen; also shows debug messages if DEBUG defined
+procedure infoln(Message: string; Level: TEventType);
+
+
+implementation
+
+procedure infoln(Message: string; Level: TEventType);
+var
+  Seriousness: string;
+begin
+  case Level of
+    etCustom: Seriousness:='Custom:';
+    etDebug: Seriousness:='Debug:';
+    etInfo: Seriousness:='Info:';
+    etWarning: Seriousness:='WARNING:';
+    etError: Seriousness:='ERROR:';
+    else Seriousness:='UNKNOWN CATEGORY!!:'
+  end;
+  if (Level<>etDebug) then
+    begin
+      if AnsiPos(LineEnding, Message)>0 then writeln(''); //Write an empty line before multiline messagse
+      writeln(Seriousness+' '+ Message); //we misuse this for info output
+      sleep(200); //hopefully allow output to be written without interfering with other output
+    end
+  else
+    begin
+      {$IFDEF DEBUG}
+      {DEBUG conditional symbol is defined using
+      Project Options/Other/Custom Options using -dDEBUG}
+      if AnsiPos(LineEnding, Message)>0 then writeln(''); //Write an empty line before multiline messagse
+      writeln(Seriousness+' '+ Message); //we misuse this for info output
+      sleep(200); //hopefully allow output to be written without interfering with other output
+      {$ENDIF DEBUG}
+    end;
+end;
+
+{ TLogger }
+
+procedure TLogger.WriteLog(Message: string; ToConsole: Boolean=false);
+begin
+  FLog.Log(etInfo, Message);
+  if ToConsole then infoln(Message,etinfo);
+end;
+
+procedure TLogger.WriteLog(EventType: TEventType;Message: string; ToConsole: Boolean=false);
+begin
+  FLog.Log(EventType, Message);
+  if ToConsole then infoln(Message,etinfo);
+end;
+
+constructor TLogger.Create;
+begin
+  FLog:=TEventLog.Create(nil);
+  FLog.LogType:=ltSystem;
+  FLog.AppendContent:=true;
+  FLog.RaiseExceptionOnError:=false; //Don't throw exceptions on log errors.
+end;
+
+destructor TLogger.Destroy;
+begin
+  FLog.Active:=false;//save WriteLog text
+  FLog.Free;
+  inherited Destroy;
+end;
+
+initialization
+begin
+  TigerLog:=TLogger.Create;
+end;
+
+finalization
+begin
+  TigerLog.Free;
+end;
+
+end.
+
