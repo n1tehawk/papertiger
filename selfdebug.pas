ee15ef622bf610a85242c608407bb3415e7a8534
reiniero <reinierolislagers@gmail.com>
2013-02-16 14:37:35 +0100

    Add cgi debugging unit

---
+++ selfdebug.pas
@@ -0,0 +1,55 @@
+unit selfdebug;
+
+//see
+//http://www.hu.freepascal.org/lists/fpc-pascal/2011-October/030752.html
+
+{
+Instructions:
+-Include unit anyware in the program.
+-Change DEBUGGER constant to match your debugger
+-For Windows, if the program to debug is running as a service (fe. CGI application
+ from apache running as a service), make sure the service is configured with
+ "Interact with desktop" checked. Failing to do so, the debugger will be started
+ without a console, break the program and hang waiting for input. Killing the debugger
+ is your only option.
+-For Linux, if the program to debug is running as a different user (fe. CGI application),
+ run "xhost +" in a terminal to enable all users to connect to xserver.
+ If needed, change DISPLAY to match your DISPLAY environment variable
+}
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,Process;
+
+implementation
+
+const
+  {$ifdef windows}
+  DEBUGGER='C:\lazarus\mingw\bin\7.3\gdb.exe';
+  {$endif}
+  {$ifdef linux}
+  DEBUGGER='gdb';
+  DISPLAY=':0.0';
+  {$endif}
+  MSWAIT=2000;
+
+var
+  AProcess: TProcess;
+
+initialization
+AProcess := TProcess.Create(nil);
+{$ifdef windows}
+AProcess.CommandLine := format('cmd /C START "Debugging %s" /WAIT "%s" "%s" %d"',[paramstr(0),debugger,paramstr(0),GetProcessID]);
+{$endif}
+{$ifdef linux}
+AProcess.CommandLine := format('xterm -display %s -T "Debugging %s" -e "%s" "%s" %d',[DISPLAY,paramstr(0),DEBUGGER,paramstr(0),GetProcessID]);
+{$endif}
+AProcess.Execute;
+sleep(MSWAIT);
+finalization
+AProcess.Free;
+end.
+
