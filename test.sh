5b0183b34da476a13633f267e0aaac932e1971be
reiniero <reiniero@localhost>
2013-02-17 10:10:36 +0100

    better test script

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+delp .
+rm tigercgi
+rm tigerserver
 hg pull && hg update
-~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus tigerserver.lpi && ./tigerserver --image=bla.tif
-
+#~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus tigerserver.lpi && ./tigerserver --image=bla.tif
+~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus tigerserver.lpi
+~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus tigercgi.lpi
+cp tigercgi /usr/lib/cgi-bin/
+# for easier troubleshooting:
+cp tigerserver /usr/lib/cgi-bin/
+cp tigerserver.ini /usr/lib/cgi-bin
+chmod ug+x /usr/lib/cgi-bin/tigercgi
+chmod ug+x /usr/lib/cgi-bin/tigerserver
+chmod ug+r /usr/lib/cgi-bin/tigerserver.ini
+ls -al /usr/lib/cgi-bin/
