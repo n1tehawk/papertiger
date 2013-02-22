4e4d840cc31c78da559af8c8dc3c06c7010058ff
reiniero <reinierolislagers@gmail.com>
2013-02-22 07:23:48 +0100

    compile fixes

--- tigercgimain.pas
+++ tigercgimain.pas
@@ -194,7 +194,7 @@ begin
         //todo: for all error messages, return 500 or something instead of 200 ok
         AResponse.Code:=404;
         AResponse.CodeText:='Error getting image file for document ID ' +
-          IntToStr(DocumentID)
+          IntToStr(DocumentID);
         AResponse.Contents.Add('<p>Error getting image file for document ID ' +
           IntToStr(DocumentID) + '</p>');
       end;
@@ -207,7 +207,7 @@ begin
     // error message
     AResponse.Code:=404;
     AResponse.CodeText:='Error retrieving image for document ID ' +
-      IntToStr(DocumentID)'l
+      IntToStr(DocumentID)';
     AResponse.Contents.Add('<p>Error retrieving image for document ID ' +
       IntToStr(DocumentID) + '</p>');
   end;
