deca9de168a26f39bcc66851b585a6d9484a6671
reiniero <reiniero@localhost>
2013-02-26 09:44:17 +0100

    cosmetic: more debugging

--- tigercgi_document.pas
+++ tigercgi_document.pas
@@ -31,15 +31,36 @@ implementation
 
 procedure TFPWebdocument.DataModuleRequest(Sender: TObject; ARequest: TRequest;
   AResponse: TResponse; var Handled: Boolean);
-// We don't define any actions but handle the request at the module level.
-// This allows handling e.g.
-// POST http://server/cgi-bin/tigercig/document/ //new doc
-// PUT http://server/cgi-bin/tigercig/document/304 //edit doc with id 304
-// GET http://server/cgi-bin/tigercig/document/304 //get document with id 304
+// We don't define any actions but handle the request at the module level before any actions would be evaluated.
+{
+Handled URLs/methods:
+GET    http://server/cgi-bin/tigercgi/document/    //list of docs
+POST   http://server/cgi-bin/tigercgi/document/    //let server create new doc, return documentid
+DELETE http://server/cgi-bin/tigercgi/document/304 //remove document with id 304
+GET    http://server/cgi-bin/tigercgi/document/304 //get document with id 304
+PUT    http://server/cgi-bin/tigercgi/document/304 //edit doc with id 304
+}
 begin
-  // First make sure the URI is valid
-  ARequest.PathInfo;
+  // Make sure the user didn't specify levels in the URI we don't support:
+  case ARequest.Method of
+    'DELETE':
+    begin
 
+    end;
+    'GET':
+    begin
+
+    end;
+    'POST':
+    begin
+
+    end;
+    'PUT':
+    begin
+
+    end;
+  end;
+  Handled:=true;
 end;
 
 initialization
