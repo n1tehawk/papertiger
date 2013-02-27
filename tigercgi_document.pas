e07bb573cf9b3106c25b9fff2b00c587aa668b6f
reiniero <reinierolislagers@gmail.com>
2013-02-27 11:24:08 +0100

    cosmetic: more debugging

--- tigercgi_document.pas
+++ tigercgi_document.pas
@@ -14,6 +14,10 @@ type
   TFPWebdocument = class(TFPWebModule)
     procedure DataModuleRequest(Sender: TObject; ARequest: TRequest;
       AResponse: TResponse; var Handled: Boolean);
+    procedure everythingAfterResponse(Sender: TObject; AResponse: TResponse);
+    procedure everythingBeforeRequest(Sender: TObject; ARequest: TRequest);
+    procedure everythingRequest(Sender: TObject; ARequest: TRequest;
+      AResponse: TResponse; var Handled: Boolean);
   private
     { private declarations }
   public
@@ -34,6 +38,7 @@ procedure TFPWebdocument.DataModuleRequest(Sender: TObject; ARequest: TRequest;
 // We don't define any actions but handle the request at the module level before any actions would be evaluated.
 {
 Handled URLs/methods:
+DELETE http://server/cgi-bin/tigercgi/document/    //delete all docs?!?!
 GET    http://server/cgi-bin/tigercgi/document/    //list of docs
 POST   http://server/cgi-bin/tigercgi/document/    //let server create new doc, return documentid
 DELETE http://server/cgi-bin/tigercgi/document/304 //remove document with id 304
@@ -41,10 +46,15 @@ GET    http://server/cgi-bin/tigercgi/document/304 //get document with id 304
 PUT    http://server/cgi-bin/tigercgi/document/304 //edit doc with id 304
 }
 begin
+  writeln('<p>document module</p>');
   // Make sure the user didn't specify levels in the URI we don't support:
   case ARequest.Method of
     'DELETE':
     begin
+      {
+      pathinfo apparently returns something like
+      /document/304
+      }
 
     end;
     'GET':
@@ -60,6 +70,25 @@ begin
 
     end;
   end;
+  Handled:=true;
+end;
+
+procedure TFPWebdocument.everythingAfterResponse(Sender: TObject;
+  AResponse: TResponse);
+begin
+
+end;
+
+procedure TFPWebdocument.everythingBeforeRequest(Sender: TObject;
+  ARequest: TRequest);
+begin
+
+end;
+
+procedure TFPWebdocument.everythingRequest(Sender: TObject; ARequest: TRequest;
+  AResponse: TResponse; var Handled: Boolean);
+begin
+  writeln('Sender classname: '+Sender.ClassName);
   Handled:=true;
 end;
 
