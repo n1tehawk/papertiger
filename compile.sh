15a47fe181f3b687144996c724ebf76d4ff9b7c0
reiniero <reinierolislagers@gmail.com>
2013-07-19 17:07:13 +0200

    cosmetic

--- compile.sh
+++ compile.sh
@@ -3,6 +3,12 @@
 # Please adjust to your own path
 hg pull
 hg update
+
+chmod ug+x hocrwrap.sh
+chmod ug+x scanwrap.sh
+chmod ug+x test.sh
+chmod ug+x textdetect.sh
+
 ~/trunk/lazarus/lazbuild --pcp="~/trunk/config_lazarus" --build-mode=Default tigercgi.lpr
 ~/trunk/lazarus/lazbuild --pcp="~/trunk/config_lazarus" --build-mode=Default tigerclient.lpr
 ~/trunk/lazarus/lazbuild --pcp="~/trunk/config_lazarus" --build-mode=Default tigerserver.lpr
