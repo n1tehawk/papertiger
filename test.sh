31bdc6bbc59169c7f4412993890a0054943418d5
reiniero <reinierolislagers@gmail.com>
2013-07-22 13:04:18 +0200

    updated test script

--- test.sh
+++ test.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 delp .
 rm tigercgi
+rm tigerclient
 rm tigerserver
 hg pull && hg update
-#~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus tigerserver.lpi && ./tigerserver --image=bla.tif
+
 echo
 echo Compiling tigerserver
 echo "**************************************************"
-~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus --build-all tigerserver.lpi
+~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus --build-mode=debug --build-all tigerserver.lpi
 echo
+
 echo
 echo Compiling tigercgi
 echo "**************************************************"
-~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus --build-all tigercgi.lpi
+~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus --build-mode=debug --build-all tigercgi.lpi
 echo
+
+echo
+echo Compiling tigerclient
+echo "**************************************************"
+~/trunk/lazarus/lazbuild --primary-config-path=~/trunk/config_lazarus --build-mode=debug --build-all tigerclient.lpi
+echo
+
+chmod ugo+rx hocrwrap.sh
+chmod ugo+rx scanwrap.sh
+chmod ugo+rx textdetect.sh
+chmod ugo+rx tigercgi
+chmod ugo+rx tigerserver
+chmod ugo+r  tigerserver.ini
 
 cp tigercgi        /usr/lib/cgi-bin/
 cp hocrwrap.sh     /usr/lib/cgi-bin/
 cp scanwrap.sh     /usr/lib/cgi-bin/
+cp textdetect.sh   /usr/lib/cgi-bin/
 cp tigerserver     /usr/lib/cgi-bin/
 cp tigerserver.ini /usr/lib/cgi-bin
 
 delp .
 
-chmod ugo+rx /usr/lib/cgi-bin/hocrwrap.sh
-chmod ugo+rx /usr/lib/cgi-bin/scanwrap.sh
-chmod ugo+rx /usr/lib/cgi-bin/tigercgi
-chmod ugo+rx /usr/lib/cgi-bin/tigerserver
-chmod ugo+r  /usr/lib/cgi-bin/tigerserver.ini
 echo "Directory /usr/lib/cgi-bin"
 ls -al /usr/lib/cgi-bin/
