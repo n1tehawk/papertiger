6b3cce9cd85db4ed0a9400516a1ba4c3f4d90c5f
reiniero <reinierolislagers@gmail.com>
2014-04-04 17:10:01 +0200

    Twain lib fixes in progress, multipart form/file upload in progress

--- 3rdparty/httpclient.pas
+++ 3rdparty/httpclient.pas
@@ -1,6 +1,7 @@
 unit HttpClient;
 
 {$mode objfpc}{$H+}
+{ Adapted from original httpclient}
 
 interface
 
@@ -15,6 +16,9 @@ type
 
   TRequestMethod = (rmGet, rmHead, rmOptions, rmPost, rmPut, rmDelete);
 
+  // Perform a POST with JSON data and a file in multipart/form data. Return response in Response
+function FileFormPostWithDataStream(const AData: TJSONData; const AURL, AFieldName: String;
+  AFile: TStream; const AFileName: string): THttpResult;
 // Perform a get etc and return JSON result data in AResponse
 function HttpRequest(const AUrl: string; out AResponse: TJSONData;
   const AMethod: TRequestMethod = rmGet): THttpResult;
@@ -31,6 +35,64 @@ function HttpRequestWithDataStream(var AData: TJSONData; const AUrl: string;
 
 
 implementation
+
+// adapted from TFPCustomHTTPClient
+function FileFormPostWithDataStream(const AData: TJSONData; const AURL, AFieldName: String;
+  AFile: TStream; const AFileName: string):THttpResult;
+const
+  CRLF=#13+#10;
+  VMethod='POST';
+var
+  VHttp: TFPHTTPClient;
+  VJSON: TJSONStringType;
+  S, Sep : string;
+  SS : TStringStream;
+  F : TFileStream;
+begin
+  VHttp := TFPHTTPClient.Create(nil);
+  try
+    VHttp.RequestHeaders.Add('Connection: Close');
+    if Assigned(AData) then
+    begin
+      VHttp.RequestBody := TMemoryStream.Create;
+      VJSON := AData.AsJSON;
+      VHttp.RequestBody.Write(Pointer(VJSON)^, Length(VJSON));
+      VHttp.RequestBody.Position := 0;
+    end;
+    // JSON part
+
+    // File part
+    Sep:=Format('%.8x_multipart_boundary',[Random($ffffff)]);
+    VHTTP.AddHeader('Content-Type','multipart/form-data; boundary='+Sep);
+    S:='--'+Sep+CRLF;
+    s:=s+Format('Content-Disposition: form-data; name="%s"; filename="%s"'+CRLF,
+      [AFieldName,ExtractFileName(AFileName)]);
+    s:=s+'Content-Type: application/octet-string'+CRLF+CRLF;
+    // Start with Content-Type...
+    SS:=TStringStream.Create(s);
+    try
+      // then add file part...
+      SS.Seek(0,soFromEnd);
+      SS.CopyFrom(AFile,AFile.Size);
+      // ... then separator
+      S:=CRLF+'--'+Sep+'--'+CRLF;
+      SS.WriteBuffer(S[1],Length(S));
+      //todo: then add json object
+      SS.Position:=0;
+      VHttp.RequestBody:=SS;
+      VHttp.HTTPMethod(VMethod, AUrl, nil, []);
+      Result.Code := VHttp.ResponseStatusCode;
+      Result.Text := VHttp.ResponseStatusText;
+    finally
+      SS.Free;
+    end;
+  finally
+    VHttp.RequestBody.Free;
+    VHttp.RequestBody := nil;
+    VHttp.Free;
+  end;
+end;
+
 
 function HttpRequest(const AUrl: string; out AResponse: TJSONData;
   const AMethod: TRequestMethod): THttpResult;
